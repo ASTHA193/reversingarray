22031e59716c695a18a6e72675cc0d8907621534
ROHIT KUMAR <rohit64229@gmail.com>
2019-10-01 00:17:45 +0530

    Added a least oomplexity code

--- reversingarray.c
+++ reversingarray.c
@@ -4,12 +4,12 @@ void main()
 {
     int a[5],i,n,j=1;
     printf( "\n enter the no in array\n");
-    for(i=0;i<5;i++)
+    for(i=0;i<n;i++)
         scanf("%d" ,&a[i]);
     printf("\nElements of Array\n");
-    for(i=0;i<5;i++)
+    for(i=0;i<n;i++)
         printf("%d " ,a[i]);
     printf("\n display elements of array\n");
-    for(i=4;i>=0;i--)
+    for(i=n;i>=0;i--)
         printf("%d  ",a[i]);
 }
